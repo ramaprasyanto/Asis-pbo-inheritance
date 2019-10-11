7f317072999360fe5c97f20ca6544bfc1d205087
Rama prasyanto <Rama.prasyanto15@gmail.com>
2019-10-11 22:51:28 +0700

    add class penjumlahan

---
+++ live-code/mainclass.java
@@ -0,0 +1,9 @@
+public class mainclass{
+    public static void main(String[] args) {
+        // penjumlahan jmlh = new penjumlahan();
+        // jmlh.jumlah(1.5,1.5);
+        // jmlh.jumlah(1,1);
+        // jmlh.cetak();
+        System.out.println("test");
+    }
+}
